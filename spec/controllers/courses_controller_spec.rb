224dfda8e2bbc143926d6b04eebf6d971906174e
tsungtingdu <tsungtingdu@gmail.com>
2020-05-27 16:03:00 +0800

    create test for GET new and POST create actions

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -31,4 +31,34 @@ RSpec.describe CoursesController, type: :controller do
       expect(response).to render_template('show')
     end
   end
+
+  describe 'GET new' do
+    it 'assigns @course' do
+      course = build(:course)
+      get :new
+      expect(assigns[:course]).to be_a_new(Course)
+    end
+  end
+
+  describe 'POST create' do
+    it 'create a new course record' do
+      course = build(:course)
+      expect do
+        post :create, params: { course: {:title => 'name', :description => 'description'}}
+        # post :create, params: { :course => attributes_for(:course) }
+      end.to change{Course.count}.by(1)
+    end
+
+    it 'redirect to index when success' do
+      course = build(:course)
+      post :create, params: { course: {:title => 'name', :description => 'description'}}
+      expect(response).to redirect_to courses_path
+    end
+
+    it 'render new when fail' do
+      course = build(:course)
+      post :create, params: { course: { :description => 'description'}}
+      expect(response).to render_template('new')
+    end
+  end
 end

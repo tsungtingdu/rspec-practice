898f42e681de4c7af6f595112e8e76e5d02433db
tsungtingdu <tsungtingdu@gmail.com>
2020-05-28 11:52:04 +0800

    create test for edit, update, destory actions

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -61,4 +61,76 @@ RSpec.describe CoursesController, type: :controller do
       expect(response).to render_template('new')
     end
   end
+
+  describe 'GET edit' do
+    it 'assign course' do
+      course = create(:course)
+      get :edit, params: {id: course.id}
+      expect(assigns[:course]).to eq(course)
+    end
+
+    it 'render tempalte' do
+      course = create(:course)
+      get :edit, params: {id: course.id}
+      expect(response).to render_template('edit')
+    end
+  end
+
+  describe 'PUT update' do
+    context 'when there is title' do
+      it 'assign @course' do
+        course = create(:course)
+        put :update, params: {id: course.id, course: {title: 'new title', description: 'new description'}}
+        expect(assigns[:course]).to eq(course)
+      end
+
+      it 'change value' do
+        course = create(:course)
+        put :update, params: {id: course.id, course: {title: 'new title', description: 'new description'}}
+        expect(assigns[:course].title).to eq('new title')
+        expect(assigns[:course].description).to eq('new description')
+      end
+
+      it 'redirect_to course_path' do
+        course = create(:course)
+        put :update, params: {id: course.id, course: {title: 'new title', description: 'new description'}}
+        expect(response).to redirect_to course_path(course)
+      end
+    end
+    
+    context 'when there is no title' do
+      it "doesn't update" do
+        course = create(:course)
+        put :update, params: {id: course.id, course: {title: '', description: 'new description'}}
+        expect(assigns[:course]).not_to eq('new description')
+      end
+
+       it "render edit" do
+        course = create(:course)
+        put :update, params: {id: course.id, course: {title: '', description: 'new description'}}
+        expect(response).to render_template('edit')
+      end
+    end
+  end
+
+  describe 'DELETE destroy' do
+    it 'assigns @course' do
+      course = create(:course)
+      delete :destroy, params: {id: course.id}
+      expect(assigns[:course]).to eq(course)
+    end
+
+    it 'delete a course' do
+      course = create(:course)
+       expect do
+        delete :destroy, params: {id: course.id}
+      end.to change{Course.count}.by(-1)
+    end
+
+    it 'redirect to index' do
+      course = create(:course)
+      delete :destroy, params: {id: course.id}
+      expect(response).to redirect_to courses_path
+    end
+  end
 end
